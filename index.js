16e99b6502b212ad14aab92060a91e216d07a2d7
Ale4ero <ale.forero10@gmail.com>
2022-11-07 12:17:47 -0500

    changed movement mechanics
    movement mechanics now rely on thrust and angles. I like this a lot more than traditional velocity direction movement, especially for a ship in space.

--- index.js
+++ index.js
@@ -11,7 +11,11 @@ class Player{
             y: 0
         }
 
-        this.rotation = 0
+        this.angle = 0;
+        this.engineOn = false;
+        this.rotatingLeft = false;
+        this.rotatingRight = false;
+        this.shooting = false;
 
         const image = new Image()
         image.src = './img/blue_orange_ship.png'
@@ -23,7 +27,7 @@ class Player{
             this.height = image.height * ratio
             this.position = {
                 x: (canvas.width / 2) - this.width / 2,
-                y: canvas.height - this.height - 20
+                y: canvas.height - this.height - 50
             }
         }
     }
@@ -34,7 +38,7 @@ class Player{
         
         c.save()
         c.translate(player.position.x + player.width/2, player.position.y + player.height / 2)
-        c.rotate(this.rotation)
+        c.rotate(this.angle)
         c.translate(-player.position.x - player.width/2, -player.position.y - player.height / 2)
         c.drawImage(
             this.image, 
@@ -44,14 +48,87 @@ class Player{
             this.height
         )
 
+        //flame
+        if(this.engineOn){
+            const fireYPos = player.position.y + player.height + 5 
+            const fireXPos = player.position.x
+            c.beginPath()
+            c.moveTo(fireXPos + player.width*.375, fireYPos)
+            c.lineTo(fireXPos + player.width*.625, fireYPos)
+            c.lineTo(fireXPos + player.width*.5, fireYPos + Math.random() * 100)
+            c.lineTo(fireXPos + player.width*.375, fireYPos)
+            c.closePath()
+            c.fillStyle = 'orange'
+            c.fill()
+        }
+
         c.restore()
     }
 
-    update(){
+    movePlayer(){
+
         if (this.image){
+            const thrust = 5
+            const turnSpeed = 5
+
             this.draw()
             this.position.x += this.velocity.x
             this.position.y += this.velocity.y
+
+            // Angle has to be in radians
+            const degToRad = Math.PI / 180;
+            // Change the position based on velocity
+            this.position.x += this.velocity.x
+            this.position.y += this.velocity.y
+
+            //right border
+            if (this.position.x + this.width >= canvas.width){
+                this.position.x -= 5
+                this.velocity.x = -this.velocity.x/2
+            }
+
+            //left border
+            if(this.position.x < 0){
+                this.position.x += 5
+                this.velocity.x = -this.velocity.x/2
+            }
+
+            //top border
+            if(this.position.y < 0){
+                this.position.y += 5
+                this.velocity.y = -this.velocity.y/2
+            }
+
+            //bottom border
+            if(this.position.y + this.height > canvas.height){
+                this.position.y -= 5
+                this.velocity.y = -this.velocity.y/2
+            }
+
+            // Turning
+            if (this.rotatingLeft) this.angle -= turnSpeed * degToRad;
+            if (this.rotatingRight) this.angle += turnSpeed * degToRad;
+
+            // Acceleration
+            if (this.engineOn) {
+                this.velocity.x += (thrust / 100) * Math.sin(this.angle);
+                this.velocity.y -= (thrust / 100) * Math.cos(this.angle);
+            }
+
+            //shooting
+            if(this.shooting){
+                projectiles.push(new Projectile({
+                    position: {
+                        x: player.position.x + player.width / 2,
+                        y: player.position.y + player.height / 2 
+                    },
+                    velocity: {
+                        x: 15 * Math.sin(this.angle),
+                        y: -15 * Math.cos(this.angle)
+                    }
+                }))
+                this.shooting = false
+            }   
         }
     }
 }
@@ -60,9 +137,7 @@ class Projectile{
     constructor({position, velocity}){
         this.position = position
         this.velocity = velocity
-
         this.radius = 5
-
     }
 
     draw(){
@@ -80,34 +155,44 @@ class Projectile{
     }
 }
 
+class meteor{
+    constructor({})
+}
+
 const player = new Player()
 const projectiles = []
 
 
-const keys = {
-    a: {
-        pressed: false 
-    },
-    d: {
-        pressed: false 
-    },
-    w: {
-        pressed: false 
-    },
-    s: {
-        pressed: false 
-    },
-    space: {
-        pressed: false 
-    }
-}
+function handleKeyInput(event) {
+    const speed = 10
+    const { key, type } = event;
+    const isKeyDown = type === 'keydown' ? true : false;
+
+    if (key === 'a'){
+        player.rotatingLeft = isKeyDown;
+        console.log('left')
+    } 
+    if (key === 'd'){
+        player.rotatingRight = isKeyDown;
+        console.log('right')
+    } 
+    if (key === 'w'){
+        player.engineOn = isKeyDown;
+        console.log('up')
+    } 
+    if (key === ' '){
+        player.shooting = isKeyDown;
+        console.log('pew')
+    } 
+  }
+
 
 function animate(){
     const speed = 10
     requestAnimationFrame(animate)
     c.fillStyle = 'black'
     c.fillRect(0, 0, canvas.width, canvas.height)
-    player.update()
+    player.movePlayer()
 
     projectiles.forEach((projectile, index)=> {
         if (projectile.position.y + projectile.radius < 0){
@@ -118,82 +203,15 @@ function animate(){
         
     })
 
-    if( keys.a.pressed && player.position.x > 0){
-        //console.log('left')
-        player.velocity.x = -speed
-        player.rotation = -.15
-    }else if(keys.d.pressed && player.position.x + player.width < canvas.width){
-        //console.log('right')
-        player.velocity.x = speed
-        player.rotation = .15
-    }else if(keys.w.pressed && player.position.y > 0){
-        player.velocity.y = -speed
-    }else if(keys.s.pressed && player.position.y + player.height < canvas.height){
-        player.velocity.y = speed
-    }else{
-        player.velocity.x = 0
-        player.velocity.y = 0
-        player.rotation = 0
-    }
+
 }
 
 
+document.addEventListener('keydown', handleKeyInput);
+document.addEventListener('keyup', handleKeyInput);
 
 animate()
 
-addEventListener('keydown', event => {
-    switch(event.key){
-        case 'a':
-            console.log('left')
-            keys.a.pressed = true
-            break
-        case 'd':
-            console.log('right')
-            keys.d.pressed = true
-            break
-        case 'w':
-            console.log('up')
-            keys.w.pressed = true
-            break
-        case 's':
-            console.log('down')
-            keys.s.pressed = true
-            break
-        case ' ':
-            console.log('pew')
-            keys.space.pressed = true
-            projectiles.push(new Projectile({
-                position: {
-                    x: player.position.x + player.width / 2,
-                    y: player.position.y
-                },
-                velocity: {
-                    x: 0,
-                    y: -15
-                }
-            }))
-            break
-    }
-})
 
-addEventListener('keyup', event => {
-    switch(event.key){
-        case 'a':
-            keys.a.pressed = false
-            break
-        case 'd':
-            keys.d.pressed = false
-            break
-        case 'w':
-            keys.w.pressed = false
-            break
-        case 's':
-            keys.s.pressed = false
-            break
-        case ' ':
-            keys.space,pressed = false
-            //console.log(projectiles)
-            break
-            
-    }
-})
+
+
